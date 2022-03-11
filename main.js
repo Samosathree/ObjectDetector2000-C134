7b34a7b8c40381d17ba77d0161b1f24bf58acde4
Samosathree <84139052+Samosathree@users.noreply.github.com>
2022-03-10 19:00:33 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function draw(){
         objectDetector.detect(video, gotResult);
         for(i=0; i < objects.length; i++){
             document.getElementById("status").innerHTML = "status : NO NO U KNOW THE STUFFS NOWS, good soup";
-            document.getElementById("objnum").innerHTML = "We detected this many thing in  the triangle : " + objects.length();
+            document.getElementById("objnum").innerHTML = "We detected this many thing in  the triangle : " + objects.length;
             fill(r,g,b);
             percent = floor(objects[i].confidence*100);
             text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
